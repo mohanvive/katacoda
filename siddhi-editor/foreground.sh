c92bcdccc56396a2360289ed91ba03f2230a83ea
Mohanadarshan V <mohan.vive@gmail.com>
2019-11-29 15:46:04 +0530

    Update foreground.sh

--- siddhi-editor/foreground.sh
+++ siddhi-editor/foreground.sh
@@ -1,9 +1,9 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-echo "Download siddhi-tooling-5.1.1.zip"
-wget https://github.com/siddhi-io/distribution/releases/download/v5.1.1/siddhi-tooling-5.1.1.zip
+echo "Download siddhi-tooling-5.2.0-m1.zip"
+wget https://github.com/mohanvive/distribution/releases/download/v5.2.0-M1/siddhi-tooling-5.2.0-m1.zip
 
-echo "Unzip siddhi-tooling-5.1.1.zip"
-unzip siddhi-tooling-5.1.1.zip
+echo "Unzip siddhi-tooling-5.2.0-m1"
+unzip siddhi-tooling-5.2.0-m1.zip
 
 echo "Start Siddhi Editor"
-./siddhi-tooling-5.1.1/bin/tooling.sh
+./siddhi-tooling-5.2.0-SNAPSHOT/bin/tooling.sh
